9f3f9debe4c7dc150394b14802e49a80f67ececa
Sean <seanmckiernan01@gmail.com>
2022-01-12 00:52:19 +0000

    working on parsing dates and times in JS

--- focustime.js
+++ focustime.js
@@ -3,24 +3,55 @@ $(document).ready(function () {
     console.log("JS loading")
 
  
-    var d = new Date();
-    var TimeNow = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    console.log('time is ', TimeNow)
-
-    var timeControl = document.querySelector('input[type="time"]');
-    timeControl.value = TimeNow;
+    var today = new Date();
+    var TimeNow = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    var TimeNow2 = today.toLocaleTimeString();
 
 
-    
+    test  = new Date();
+    const options = { dateStyle: 'short' };
+    const dateToday = test.toLocaleString('en-ie', options);
+    console.log('test', dateToday)
+
     document.getElementById('TimeNow').innerHTML = `${TimeNow}`;
 
     $(document).on("click", "#GoButton", function () {
       
+        
     console.log("go!")
     TimeEndofDay = $("#EndOfDay").val() 
     TimeLunch = $("#LunchBreak").val() 
     TimeFocus = $("#FocusSession").val() 
     TimeEmail = $("#EmailTime").val() 
+
+    console.log("today = ", TimeNow2)
+    console.log('End = ', TimeEndofDay)
+    console.log('Lunch = ', TimeLunch)
+    console.log('Focus = ', TimeFocus)
+    console.log('Email = ', TimeEmail)
+    console.log('Now = ', TimeNow)
+
+
+    TimeEndOfDayHH = TimeEndofDay.split(":")[0]
+    TimeEndOfDayMM = TimeEndofDay.split(":")[1]
+
+    TimeLunchHH = TimeLunch.split(":")[0]
+    TimeLunchMM = TimeLunch.split(":")[1]
+
+
+    var TimeDateEndOfDay = new Date(null, null, null, TimeEndOfDayHH, TimeEndOfDayMM);
+    var TimeDateLunch = new Date(null, null, null, TimeLunchHH, TimeLunchMM);
+    
+    console.log(TimeDateEndOfDay, "-",TimeDateLunch)
+    var NetofLunch  = Math.abs(TimeDateEndOfDay - TimeDateLunch);
+
+
+    const date = new Date(NetofLunch);
+
+    alert(`${date.getMinutes()}`);
+        console.log('Net of lunch = ', NetofLunch)
+
+
 
 
 });
